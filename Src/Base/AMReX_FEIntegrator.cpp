f9af9f23790058d9ed4d709e779d019bf3b7ed0f
Donald E. Willcox <eugene.willcox@gmail.com>
2020-06-04 15:33:19 -0700

    For FE Integrator, use std::swap() instead of MultiFab::Copy() and do not include grow cells in saxpy with RHS.

--- Src/Base/AMReX_FEIntegrator.cpp
+++ Src/Base/AMReX_FEIntegrator.cpp
@@ -13,15 +13,15 @@ FEIntegrator::FEIntegrator(amrex::MultiFab& S_old_external,
 Real FEIntegrator::advance(const Real timestep)
 {
     // Assume before advance() that S_new is valid data at the current time ("time" argument)
-    // So we update S_old by copying the current state.
-    MultiFab::Copy(S_old, S_new, 0, 0, S_old.nComp(), S_old.nGrow());
+    // So we update S_old by swapping the old/new state.
+    std::swap(S_old, S_new);
 
     // F_tmp = RHS(S_old, t_old)
     MultiFab& F_tmp = *F_tmp_ptr;
     rhs(F_tmp, S_old, time);
 
     // S_new += timestep * dS/dt
-    MultiFab::Saxpy(S_new, timestep, F_tmp, 0, 0, S_new.nComp(), S_new.nGrow());
+    MultiFab::Saxpy(S_new, timestep, F_tmp, 0, 0, S_new.nComp(), 0);
 
     // Call the post-update hook for S_new
     post_update(S_new);
